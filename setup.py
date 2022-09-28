23f42916f027d6631efc292c54a66129b15e131b
Eren Doğruca <edogruca@hotmail.com>
2022-09-28 16:31:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
   packages=setuptools.find_packages(),
   install_requires=[
       'opencv-python>=4.1.0.25',
-      'torch',
+      'torch==1.9.0+cpu',
       'onnx',
       'onnxruntime',
       'Pillow>=6.0.0',
